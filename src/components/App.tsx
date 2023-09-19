4830739b464458e46f2fcf9f1f0938ec875230f0
James Cleveland <jc@blit.cc>
2023-09-19 16:36:48 +0100

    adding dummy store

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,18 @@
-function App() {
+import { observer } from "mobx-react-lite";
+import { store } from "../state";
+
+export const App = observer(() => {
+  const { test } = store;
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <p className="m-10 bg-black text-white">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="m-10 bg-black text-white">Click on the Vite and React logos to learn more</p>
+      <p>{test.no}</p>
+      <button className="btn" onClick={() => test.increment()}>
+        Inc
+      </button>
     </>
   );
-}
+});
 
 export default App;
