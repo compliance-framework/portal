dd22fbd353a46e552c08d1b793a7cd5356abede3
James Cleveland <jc@blit.cc>
2023-09-19 17:18:06 +0100

    add basic test

---
+++ src/components/App.test.tsx
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-unused-vars
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import App from "./App";
+
+describe("App", () => {
+  it("Vite to be in document", () => {
+    render(<App />);
+    expect(screen.getByText("Hello world!")).toBeInTheDocument();
+  });
+});
