45ffd09585f518e7d4fc70f25ae51cbe4a3224b0
James Cleveland <jc@blit.cc>
2023-11-03 16:55:11 +0000

    subtle button hover effect

--- src/components/Common/Button.tsx
+++ src/components/Common/Button.tsx
@@ -8,7 +8,7 @@ export function Button({ icon, label, onClick }: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="flex flex-row items-center justify-center rounded border border-indigo-850 px-2 py-1">
+      className="flex flex-row items-center justify-center rounded border border-indigo-850 px-2 py-1 hover:bg-cloud-burst-500">
       <img src={icon} alt={label} className="mr-2 w-4" />
       {label}
     </button>
