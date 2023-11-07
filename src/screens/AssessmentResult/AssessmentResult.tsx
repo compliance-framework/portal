f3ac82752aed5c88517137cc17d3f5297840de45
James Cleveland <jc@blit.cc>
2023-11-07 17:03:31 +0000

    output some JSON from API

--- src/screens/AssessmentResult/AssessmentResult.tsx
+++ src/screens/AssessmentResult/AssessmentResult.tsx
@@ -1,7 +1,8 @@
 import { observer } from "mobx-react-lite";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { AppContainer } from "../../components/AppContainer/AppContainer.tsx";
+import { PlanApi } from "../../services/configuration-service";
 import { Summary } from "./AssessmentResultSummary/Summary.tsx";
 import { ComplianceStatus } from "./ComplianceStatus/ComplianceStatus.tsx";
 import { FindingList } from "./FindingList.tsx";
@@ -11,14 +12,26 @@ interface AssessmentResultProps {
   children?: ReactNode;
 }
 
+const planApi = new PlanApi({
+  basePath: "http://localhost:8080/api",
+});
+
 export const AssessmentResult = observer<AssessmentResultProps>(() => {
   const { id } = useParams();
 
+  const [data, setData] = useState<any>(null);
   if (!id) return null;
+  useEffect(() => {
+    (async () => {
+      const result = await planApi.planIdResultsGet(id);
+      setData(result.data);
+    })();
+  }, [id]);
 
   return (
     <AppContainer>
       <div className="m-0 grid grid-cols-2 gap-2 p-0">
+        <pre className="text-xs">{JSON.stringify(data, null, 2)}</pre>
         <Summary id={id} />
         <FindingList />
         <ComplianceStatus id={id} />
