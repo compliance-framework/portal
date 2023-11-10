e45ed6739ed0ba2c9434ff40c16680870a5b18cb
James Cleveland <jc@blit.cc>
2023-11-10 12:42:17 +0000

    fix errs

--- src/screens/Dashboard/Dashboard.tsx
+++ src/screens/Dashboard/Dashboard.tsx
@@ -1,29 +1,22 @@
 import { observer } from "mobx-react-lite";
 import { ReactNode } from "react";
-import { useTranslation } from "react-i18next";
+import { useParams } from "react-router-dom";
 import { AppContainer } from "../../components/AppContainer/AppContainer.tsx";
-import { Widget } from "../../components/Common/Widget.tsx";
+import { ComplianceStatusByTargets } from "../PlanResults/ComplianceStatusByTargets/ComplianceStatusByTargets.tsx";
+import { Summary } from "../PlanResults/PlanResultSummary/Summary.tsx";
 
 interface DashboardProps {
   children?: ReactNode;
 }
-const toolbar = (
-  <button
-    onClick={() => console.log("summary clicked")}
-    className="flex flex-row items-center justify-center rounded border border-indigo-850 px-2 py-1">
-    <img src={"/settings.png"} alt="Settings" className="mr-2 w-4" />
-    Settings
-  </button>
-);
-
-export const Dashboard = observer<DashboardProps>(({ children }) => {
-  const { t } = useTranslation();
+export const Dashboard = observer<DashboardProps>(() => {
+  const { id = "todo", resultId = "todo" } = useParams();
 
   return (
     <AppContainer>
-      <Widget title={t("widgets.summary")} toolbar={toolbar}>
-        {children}
-      </Widget>
+      <div className="m-0 grid grid-cols-2 gap-2 p-0">
+        <Summary id={id} resultId={resultId} />
+        <ComplianceStatusByTargets id={id} resultId={resultId} />
+      </div>
     </AppContainer>
   );
 });
