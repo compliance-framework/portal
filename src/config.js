fe76ab6f9141eced8cf266a6d9afd864c93c6395
James Cleveland <jc@blit.cc>
2023-11-10 15:46:21 +0000

    format??

--- src/config.js
+++ src/config.js
@@ -3,10 +3,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.API = exports.ConfigurationApiConfig = void 0;
 var configurationService = require("./services/configuration-service");
 exports.ConfigurationApiConfig = new configurationService.Configuration({
-    basePath: "http://localhost:8080/api",
+  basePath: "http://localhost:8080/api",
 });
 exports.API = {
-    configurationService: {
-        plan: new configurationService.PlanApi(exports.ConfigurationApiConfig),
-    },
+  configurationService: {
+    plan: new configurationService.PlanApi(exports.ConfigurationApiConfig),
+  },
 };
--- src/services/configuration-service/base.js
+++ src/services/configuration-service/base.js
@@ -12,21 +12,31 @@
  * https://openapi-generator.tech
  * Do not edit the class manually.
  */
-var __extends = (this && this.__extends) || (function () {
+var __extends =
+  (this && this.__extends) ||
+  (function () {
     var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
+      extendStatics =
+        Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array &&
+          function (d, b) {
+            d.__proto__ = b;
+          }) ||
+        function (d, b) {
+          for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p];
+        };
+      return extendStatics(d, b);
     };
     return function (d, b) {
-        if (typeof b !== "function" && b !== null)
-            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+      if (typeof b !== "function" && b !== null)
+        throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+      extendStatics(d, b);
+      function __() {
+        this.constructor = d;
+      }
+      d.prototype = b === null ? Object.create(b) : ((__.prototype = b.prototype), new __());
     };
-})();
+  })();
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.RequiredError = exports.BaseAPI = exports.COLLECTION_FORMATS = exports.BASE_PATH = void 0;
 var axios_1 = require("axios");
@@ -36,10 +46,10 @@ exports.BASE_PATH = "http://localhost".replace(/\/+$/, "");
  * @export
  */
 exports.COLLECTION_FORMATS = {
-    csv: ",",
-    ssv: " ",
-    tsv: "\t",
-    pipes: "|",
+  csv: ",",
+  ssv: " ",
+  tsv: "\t",
+  pipes: "|",
 };
 /**
  *
@@ -47,18 +57,22 @@ exports.COLLECTION_FORMATS = {
  * @class BaseAPI
  */
 var BaseAPI = /** @class */ (function () {
-    function BaseAPI(configuration, basePath, axios) {
-        if (basePath === void 0) { basePath = exports.BASE_PATH; }
-        if (axios === void 0) { axios = axios_1.default; }
-        this.basePath = basePath;
-        this.axios = axios;
-        if (configuration) {
-            this.configuration = configuration;
-            this.basePath = configuration.basePath || this.basePath;
-        }
+  function BaseAPI(configuration, basePath, axios) {
+    if (basePath === void 0) {
+      basePath = exports.BASE_PATH;
     }
-    return BaseAPI;
-}());
+    if (axios === void 0) {
+      axios = axios_1.default;
+    }
+    this.basePath = basePath;
+    this.axios = axios;
+    if (configuration) {
+      this.configuration = configuration;
+      this.basePath = configuration.basePath || this.basePath;
+    }
+  }
+  return BaseAPI;
+})();
 exports.BaseAPI = BaseAPI;
 /**
  *
@@ -67,13 +81,13 @@ exports.BaseAPI = BaseAPI;
  * @extends {Error}
  */
 var RequiredError = /** @class */ (function (_super) {
-    __extends(RequiredError, _super);
-    function RequiredError(field, msg) {
-        var _this = _super.call(this, msg) || this;
-        _this.field = field;
-        _this.name = "RequiredError";
-        return _this;
-    }
-    return RequiredError;
-}(Error));
+  __extends(RequiredError, _super);
+  function RequiredError(field, msg) {
+    var _this = _super.call(this, msg) || this;
+    _this.field = field;
+    _this.name = "RequiredError";
+    return _this;
+  }
+  return RequiredError;
+})(Error);
 exports.RequiredError = RequiredError;
