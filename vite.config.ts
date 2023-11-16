e03d5df967c176d892f5529fa43e103eebad8d1f
James Cleveland <jc@blit.cc>
2023-11-16 15:51:25 +0000

    some nice chunking

--- vite.config.ts
+++ vite.config.ts
@@ -15,4 +15,17 @@ export default defineConfig({
     port: 8081,
     strictPort: true,
   },
+  build: {
+    assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom", "react-router-dom"],
+          state: ["mobx", "mobx-react-lite", "mobx-state-tree"],
+          charts: ["recharts", "react-gauge-chart-es"],
+          i18n: ["i18next", "react-i18next"],
+        },
+      },
+    },
+  },
 });
